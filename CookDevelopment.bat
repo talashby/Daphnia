7fce47cc7c3b6fffb07ace3ebdc5fc692d37f7af
Nikolay Ukhov <ukhov@mail.ru>
2020-01-28 21:36:56 +0300

    batches for cooking

---
+++ CookDevelopment.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set WORKSPACE=%~dp0.
+set PROJECT_NAME=Daphnia
+set UE4_ROOT=D:\UnrealSources
+
+call "%UE4_ROOT%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%WORKSPACE%/%PROJECT_NAME%.uproject" -noP4 -platform=Win64 -platform=Win64 -clientconfig=Development -serverconfig=Development -noclean -build -cook -allmaps -utf8output -stage -pak -compressed -archive -archivedirectory="%WORKSPACE%/Archive"
